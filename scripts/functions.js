64c6e11f837e519e0fb0413bf992cf6bacc54432
zodiacbalance <zodiacweight@gmail.com>
2017-04-15 15:39:19 +0300

    ...

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,8 +1,8 @@
 /**
  * Created by User on 14.04.2017.
  */
-/* * */
-function getData(title) {
+
+function getData() {
     var path = "data.json";
     // 1. Создаём новый объект XMLHttpRequest
     var xhr = new XMLHttpRequest();
@@ -16,12 +16,12 @@ function getData(title) {
             // обработать ошибку
         } else {
             var data = JSON.parse(xhr.responseText);
-            window.data = {};
-            window.data[title] = data[title]; // определены оба объекта 'Black_parody', 'Xmarine'
+            //window.data = {};
+            // window.data[title] = data[title]; // определены оба объекта 'Black_parody', 'Xmarine'
             //console.log('window[key]=>', window[key]);
             // save pages
-            console.log("trying: ", data[title]);
-            return data[title];
+            console.log("данные в теле функции: ", data);
+            return data;
         }
     };
     xhr.onerror = function (event) {
@@ -29,60 +29,15 @@ function getData(title) {
     };
 }
 
-
-function checkJsonData(title) {
-    // key: supergirl или Black mamba.
-    var defer = $.Deferred(),
-        cnt = 0;
-    // вызывается многократно
-    var sttm = setInterval(function () {
-        ++cnt;
-        if (window.data[title]) {
-            // this передан через .bind
-            this.data = window.data[title]; // (xmarineModel | black_parodyModel).play_object
-            defer.resolve(this.data);
-            clearInterval(sttm);
-        }
-        if (cnt >= 60) {
-            console.warn('Cannot get file');
-            defer.reject("The content is not here yet.");
-            clearInterval(sttm);
-        }
-    }.bind(this), 200);
-    return defer.promise();
-}
-
 var getAccessToData = (function () {
     var data = {};
-    /* var getData = function (title) {
-     var path="data.json";
-     // 2. Конфигурируем его: GET-запрос на URL 'Xmarine.json'
-     xhr.open('GET', path);// путь к тому или иному json
-     // 3. Отсылаем запрос
-     xhr.send();
-     xhr.onload = function () {
-     // 4. Если код ответа сервера не 200, то это ошибка
-     if (xhr.status != 200) {
-     // обработать ошибку
-     } else {
-     var data = JSON.parse(xhr.responseText);
-     window.data={};
-     window.data[title] = data[title]; // определены оба объекта 'Black_parody', 'Xmarine'
-     //console.log('window[key]=>', window[key]);
-     // save pages
-     return data[title];
-     }
-     };
-     xhr.onerror = function (event) {
-     console.log(event);
-     };
-     }; */
     return {
-        getData: function (title) {
+        retreiveValue: function (title) {
             return data[title];
         },
         addData: function (title) {
-            data[title] = getData(title);
+            data = getData(title);
+            console.log("data вне функции = ", data);
         }
     }
 }());
