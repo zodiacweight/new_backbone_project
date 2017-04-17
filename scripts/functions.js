11efbf5268c371c497bada4ee116aaabdce734cc
Liza <zodiacweight@gmail.com>
2017-04-17 14:24:14 +0000

    Data is defined

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,31 +2,47 @@
  * Created by User on 14.04.2017.
  */
 
-function getData() {
-    var path = "data.json";
-    // 1. Создаём новый объект XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    // 2. Конфигурируем его: GET-запрос на URL 'Xmarine.json'
-    xhr.open('GET', path);// путь к тому или иному json
-    // 3. Отсылаем запрос
-    xhr.send();
-    xhr.onload = function () {
-        // 4. Если код ответа сервера не 200, то это ошибка
-        if (xhr.status != 200) {
-            // обработать ошибку
-        } else {
-            var data = JSON.parse(xhr.responseText);
-            //window.data = {};
-            // window.data[title] = data[title]; // определены оба объекта 'Black_parody', 'Xmarine'
-            //console.log('window[key]=>', window[key]);
-            // save pages
-            console.log("данные в теле функции: ", data);
-            return data;
-        }
-    };
-    xhr.onerror = function (event) {
-        console.log(event);
-    };
+function getData(title) {
+        var promise = new Promise(function(resolve, reject){
+        var path = "data.json";
+        // 1. Создаём новый объект XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+        // 2. Конфигурируем его: GET-запрос на URL 'Xmarine.json'
+        xhr.open('GET', path);// путь к тому или иному json
+        // 3. Отсылаем запрос
+        xhr.send();
+        xhr.onload = function () {
+            // 4. Если код ответа сервера не 200, то это ошибка
+            if (xhr.status != 200) {
+                // обработать ошибку
+            } else {
+                var cnt=0, int = setInterval(function(){
+                    var data = JSON.parse(xhr.responseText);
+                    //window.data = {};
+                    // window.data[title] = data[title]; // определены оба объекта 'Black_parody', 'Xmarine'
+                    //console.log('window[key]=>', window[key]);
+                    // save pages
+                    cnt++;
+                    if(data[title]){
+                        resolve(data[title]);
+                        console.log("данные в теле функции: ", data);
+                        clearInterval(int);
+                    }
+                    else {
+                        if(cnt>15){
+                            reject("Данные не получены");
+                            clearInterval(int);
+                        }
+                        
+                    }  
+                }, 100);
+            }
+        };
+        xhr.onerror = function (event) {
+            console.log(event);
+        };
+    });
+    return promise; // функция должна возвращать promise.
 }
 
 
@@ -37,8 +53,14 @@ var getAccessToData = (function () {
             return data[title];
         },
         addData: function (title) {
-            data = getData(title);
-            console.log("data вне функции = ", data);
+            getData(title).then(
+                function(result){
+                    console.log("data вне функции = ", result);
+                },
+                function (message){
+                    console.log(message);
+                }
+            );
         }
     }
 }());
